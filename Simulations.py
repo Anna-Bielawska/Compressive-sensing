5f64dcedbf5ddfee415f92237058dd85a0c56ff1
ania_bielawska <249825@student.pwr.edu.pl>
2021-09-26 17:17:35 +0200

    Copyright and initial information

---
+++ Simulations.py
@@ -0,0 +1,12 @@
+'''
+   Author: Anna Bielawska
+   Field of study: Mathematics and Statistics
+   Dissertation: Compressive sensing - the new sampling method.
+   Institution of studies: Wrocław University of Science and Technology
+   Year of thesis defence: 2021
+   
+   The code written below was used for computer simulations included in the diploma thesis.
+   It contains 4 original part-implementations of optimization algorithms: Basis Pursuit, Basic Thresholding, 
+   Iterative Hard Thresholding, Hard Thresholding Pursuit, with use of scipy library. 
+   There already existed an implementation of the 5th algorithm of interest, OMP - Orthogonal Matching Pursuit.
+'''
