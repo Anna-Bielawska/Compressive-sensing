a3ffd90c1145a74bf6720a2efe0c1261214976d1
ania_bielawska <249825@student.pwr.edu.pl>
2021-09-26 17:27:49 +0200

    Hard thresholding pursuit algorithm

--- Simulations.py
+++ Simulations.py
@@ -141,3 +141,47 @@ def IHT(A, y, s, Its=200, tol=0.0001):
             break
 
     return xhat
+
+
+############################################ HTP - Hard Thresholding Pursuit ############################################
+
+def HTP(A, y, s, Its=100, tol=0.0001):
+    
+    '''Hard thresholding pursuit algorithm as in the thesis,
+       recovers a sparse vector xhat from y and A;
+       A - sampling matrix,
+       y - vector of measurements,
+       s - number of non-zero coefficients in the signal to be reconstructed,
+       Its - number of maximum algorithm iterations,
+       tol = tolerance (error)'''
+
+    # A function to be minimized
+    fun = lambda x: sum((y - A.dot(x))**2)
+    
+    # Length of original signal
+    N = np.shape(A)[1]
+
+    # Initial estimate
+    xhat = np.zeros(N)
+
+    for t in range(Its):
+        
+        # Pre-threshold value
+        gamma = xhat + np.dot(np.transpose(A), y-A.dot(xhat))
+
+        # Find the dictionary in k-th step
+        D_k = Ls(gamma, s, N)
+        
+        # Constraints for the problem
+        cons = {'type': 'eq', 'fun': lambda x: x[D_k]}
+
+        sol = scipy.optimize.minimize(fun, x0 = xhat, method='SLSQP', constraints=cons, tol = tol)    
+        
+        # Estimated solution
+        xhat = sol.x
+
+        # Stopping criteria
+        if sum(abs(y-A.dot(xhat))) < tol:
+            break
+
+    return xhat
